e56623555cf8ac5e60bc1b658fbcea92bab74ec3
Kevin <kchen7e@me.com>
2022-12-02 00:33:47 +1000

    disable context test

--- src/test/java/com/mealprep/MealPrep/MealPrepApplicationTests.java
+++ src/test/java/com/mealprep/MealPrep/MealPrepApplicationTests.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MealPrepApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
